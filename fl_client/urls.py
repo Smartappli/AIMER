d23b7624779e63a115c3e8fe5e013b0b3ee00b8d
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2023-12-10 23:06:04 +0100

    dev

--- fl_client/urls.py
+++ fl_client/urls.py
@@ -5,6 +5,7 @@ app_name = 'fl_client'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', include('django.contrib.auth.urls')),
     path('data_processing/', views.data_processing, name='data_processing'),
     path('machine_learning/', views.machine_learning, name='machine_learning'),
     path('deep_learning/', views.deep_learning, name='deepl_learning'),
