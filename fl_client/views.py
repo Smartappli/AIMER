46377581abde35c272cf61c1eabc41b5f8bd830e
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2023-12-10 01:16:59 +0100

    dev

--- fl_client/views.py
+++ fl_client/views.py
@@ -4,3 +4,14 @@ def index(request):
     logo = ['share', 'hospital', 'data', 'cpu', 'gpu']
     return render(request, "core/index.html", {"logo": logo})
 
+def deep_learning(request):
+    logo = ['share', 'hospital', 'data', 'cpu', 'gpu']
+    return render(request, "deep_learning/deep_learning.html", {"logo": logo})
+
+def machine_learning(request):
+    logo = ['share', 'hospital', 'data', 'cpu', 'gpu']
+    return render(request, "machine_learning/machine_learning.html", {"logo": logo})
+
+def natural_language_processing(request):
+    logo = ['share', 'hospital', 'data', 'cpu', 'gpu']
+    return render(request, "machine_learning/machine_learning.html", {"logo": logo})
