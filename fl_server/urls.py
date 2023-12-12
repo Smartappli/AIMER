9c194094a6886d702feaab1ec1c934e37817c45e
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2023-12-12 13:41:41 +0100

    Dashboard server - part 1

--- fl_server/urls.py
+++ fl_server/urls.py
@@ -6,4 +6,8 @@ app_name = 'fl_server'
 urlpatterns = [
     # path('', views.model_list, name='model_list'),
     path('', views.dashboard, name='dashboard'),
+    path('server_projects/', views.server_projects, name='projects'),
+    path('server_stakeholders/', views.server_stakeholders, name='stakeholders'),
+    path('server_monitoring/', views.server_monitoring, name='monitoring'),
+    path('server_management/', views.server_management, name='management')
 ]
--- fl_server/views.py
+++ fl_server/views.py
@@ -1,14 +1,38 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 # from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 # from fl_server.forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-#from .models import Model
+# from .models import Model
 # from django.contrib import messages
 
 
-@login_required
 def dashboard(request):
     return render(request,
-                  'account/dashboard.html',
+                  'server/server_dashboard.html',
                   {'section': 'Dashboard'})
+
+
+def server_projects(request):
+    return render(request,
+                  'server/server_projects.html',
+                  {'section': 'projects'})
+
+
+def server_stakeholders(request):
+    return render(request,
+                  'server/server_stakeholders.html',
+                  {'section': 'stakeholders'})
+
+
+
+def server_monitoring(request):
+    return render(request,
+                  'server/server_monitoring.html',
+                  {'section': 'monitoring'})
+
+
+def server_management(request):
+    return render(request,
+                  'server/server_management.html',
+                  {'section': 'management'})
