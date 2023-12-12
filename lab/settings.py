d8dd1dfe8b0f7b17edcc7755a91ce6d9200d7dc2
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2023-12-12 12:43:24 +0100

    transfert login/registration etc from fl_server to fl_client - step 2

--- lab/settings.py
+++ lab/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
