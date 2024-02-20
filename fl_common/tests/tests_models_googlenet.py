a5b64c15209709ea9bff57c6169acd156f782a06
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-02-20 14:11:35 +0100

    Refactoring Models Unit Test - G

--- fl_common/tests/tests_models_googlenet.py
+++ fl_common/tests/tests_models_googlenet.py
@@ -1,60 +1,12 @@
 import os
 import torch.nn as nn
 from django.test import TestCase
-from fl_common.models.gcvit import get_gcvit_model
 from fl_common.models.googlenet import get_googlenet_model
 
 os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "true"
 
 
 class ProcessingPartGTestCase(TestCase):
-    """Gcvit Models Unit Tests"""
-    def test_known_gcvit_types(self):
-        """
-        Test the get_gcvit_model function with known GCVIT types.
-
-        For each known GCVIT type, this test checks if the function returns a non-None GCVIT model
-        when provided with that type.
-
-        Parameters:
-        - None
-
-        Returns:
-        - None
-
-        Raises:
-        - AssertionError: If the function returns None for any known GCVIT type.
-        """
-        known_types = [
-            "gcvit_xxtiny", "gcvit_xtiny", "gcvit_tiny",
-            "gcvit_small", "gcvit_base"
-        ]
-
-        for gcvit_type in known_types:
-            with self.subTest(gcvit_type=gcvit_type):
-                gcvit_model = get_gcvit_model(gcvit_type, num_classes=10)
-                self.assertIsNotNone(gcvit_model)
-
-    def test_unknown_gcvit_type(self):
-        """
-        Test the get_gcvit_model function behavior when provided with an unknown GCVIT type.
-
-        This test checks if the function raises a ValueError when it is called with an unknown GCVIT type.
-
-        Parameters:
-        - None
-
-        Returns:
-        - None
-
-        Raises:
-        - AssertionError: If the function does not raise a ValueError for an unknown GCVIT type.
-        """
-        unknown_type = "unknown_gcvit_type"
-
-        with self.assertRaises(ValueError):
-            get_gcvit_model(unknown_type, num_classes=10)
-
     """GoogleNet Model Unit Tests"""
     def test_get_googlenet_model(self):
         """
