5553528a802406c5a2f9391884d4fd2f16c5cea9
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-02-20 17:12:11 +0100

    Refactoring Test models - part V

---
+++ fl_common/tests/tests_models_vision_transformer.py
@@ -0,0 +1,69 @@
+import os
+import torch.nn as nn
+from django.test import TestCase
+from fl_common.models.vision_transformer import get_vision_model
+
+os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "true"
+
+
+class ProcessingVisionTransformerTestCase(TestCase):
+    """Vision Transformer Model Unit Tests"""
+    def test_get_vision_model(self):
+        """
+        Test case for obtaining various Vision Transformer models.
+
+        Raises:
+            AssertionError: If any of the assertions fail.
+        """
+        # List of Vision Transformer model types to test
+        vision_types = ['ViT_B_16', 'ViT_B_32', 'ViT_L_16', 'ViT_L_32', 'ViT_H_14']
+        num_classes = 10  # You can adjust the number of classes as needed
+
+        # Loop through each Vision Transformer model type
+        for vision_type in vision_types:
+            with self.subTest(vision_type=vision_type):
+                # Get the Vision Transformer model for testing
+                model = get_vision_model(vision_type, num_classes)
+                # Check if the model is an instance of torch.nn.Module
+                self.assertIsInstance(model, nn.Module, msg=f'get_vision_model {vision_type} KO')
+
+    def test_vision_unknown_architecture(self):
+        """
+        Test case for handling unknown Vision Transformer architecture in get_vision_model function.
+
+        Raises:
+            AssertionError: If the assertion fails.
+            ValueError: If an unknown Vision Transformer architecture is provided.
+        """
+        vision_type = 'UnknownArchitecture'
+        num_classes = 10
+
+        with self.assertRaises(ValueError) as context:
+            # Attempt to get a Vision Transformer model with an unknown architecture
+            get_vision_model(vision_type, num_classes)
+
+        self.assertEqual(
+            str(context.exception),
+            f'Unknown Vision Transformer Architecture: {vision_type}'
+        )
+
+    """
+    def test_vision_nonlinear_last_layer(self):
+        # Provide a vision_type with a known non-linear last layer
+        vision_type = 'ViT_B_16'
+        num_classes = 10
+
+        # Override the last layer with a non-linear layer for testing purposes
+        vision_model = get_vision_model(vision_type, num_classes)
+        vision_model.heads[-1] = nn.ReLU()
+
+        with self.assertRaises(ValueError) as context:
+            # Try to create the vision model again
+            get_vision_model(vision_type, num_classes)
+
+        # Check if the raised ValueError contains the expected message
+        self.assertIn(
+            'The last layer is not a linear layer.',
+            str(context.exception)
+        )
+    """
---
+++ fl_common/tests/tests_models_vovnet.py
@@ -0,0 +1,55 @@
+import os
+from django.test import TestCase
+from fl_common.models.vovnet import get_vovnet_model
+
+os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "true"
+
+
+class ProcessingVovnetTestCase(TestCase):
+    """Vovnet Models Unit Tests"""
+    def test_known_vovnet_architecture(self):
+        """
+        Test the function with known architectures.
+
+        For each known architecture, it checks that no exceptions are raised,
+        and the returned model is not None.
+
+        Supported architectures:
+        - 'vovnet39a'
+        - 'vovnet57a'
+        - 'ese_vovnet19b_slim_dw'
+        - 'ese_vovnet19b_dw'
+        - 'ese_vovnet19b_slim'
+        - 'ese_vovnet39b'
+        - 'ese_vovnet57b'
+        - 'ese_vovnet99b'
+        - 'eca_vovnet39b'
+        - 'ese_vovnet39b_evos'
+        """
+        architectures = [
+            'vovnet39a',
+            'vovnet57a',
+            'ese_vovnet19b_slim_dw',
+            'ese_vovnet19b_dw',
+            'ese_vovnet19b_slim',
+            'ese_vovnet39b',
+            'ese_vovnet57b',
+            'ese_vovnet99b',
+            'eca_vovnet39b',
+            'ese_vovnet39b_evos'
+        ]
+
+        for arch in architectures:
+            with self.subTest(architecture=arch):
+                # Ensure no exceptions are raised
+                model = get_vovnet_model(arch, num_classes=10)
+                self.assertIsNotNone(model)
+
+    def test_unknown_vovnet_architecture(self):
+        """
+        Test the function with known architectures.
+        Ensure that no exceptions are raised, and the returned model is not None.
+        """
+        # Test the function with an unknown architecture
+        with self.assertRaises(ValueError):
+            get_vovnet_model('unknown_architecture', num_classes=10)
