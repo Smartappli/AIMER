31eb93fe607540ac7e589830c615e792c42401b8
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-03-03 19:44:25 +0100

    Tests fix

--- fl_common/models/vision_transformer_hybrid.py
+++ fl_common/models/vision_transformer_hybrid.py
@@ -3,15 +3,15 @@ from timm import create_model
 
 def get_vision_transformer_hybrid_model(vision_transformer_hybrid_type, num_classes):
     if vision_transformer_hybrid_type == 'vit_tiny_r_s16_p8_224':
-        vision_transformer_hybrid_model = create_model('vision_transformer_hybrid_', pretrained=True, num_classes=num_classes)
+        vision_transformer_hybrid_model = create_model('vit_tiny_r_s16_p8_224', pretrained=True, num_classes=num_classes)
     elif vision_transformer_hybrid_type == 'vit_tiny_r_s16_p8_384':
-        vision_transformer_hybrid_model = create_model('vit_tiny_r_s16_p8_384', pretrained=True, num_classes=num_classes)
+        vision_transformer_hybrid_model = create_model('vit_tiny_r_s16_p8_384', pretrained=False, num_classes=num_classes)
     elif vision_transformer_hybrid_type == 'vit_small_r26_s32_224':
-        vision_transformer_hybrid_model = create_model('vit_small_r26_s32_224', pretrained=True, num_classes=num_classes)
+        vision_transformer_hybrid_model = create_model('vit_small_r26_s32_224', pretrained=False, num_classes=num_classes)
     elif vision_transformer_hybrid_type == 'vit_small_r26_s32_384':
         vision_transformer_hybrid_model = create_model('vit_small_r26_s32_384', pretrained=True, num_classes=num_classes)
     elif vision_transformer_hybrid_type == 'vit_base_r26_s32_224':
-        vision_transformer_hybrid_model = create_model('vit_base_r26_s32_224', pretrained=True, num_classes=num_classes)
+        vision_transformer_hybrid_model = create_model('vit_base_r26_s32_224', pretrained=False, num_classes=num_classes)
     elif vision_transformer_hybrid_type == 'vit_base_r50_s16_224':
         vision_transformer_hybrid_model = create_model('vit_base_r50_s16_224', pretrained=True, num_classes=num_classes)
     elif vision_transformer_hybrid_type == 'vit_base_r50_s16_384':
@@ -21,13 +21,13 @@ def get_vision_transformer_hybrid_model(vision_transformer_hybrid_type, num_clas
     elif vision_transformer_hybrid_type == 'vit_large_r50_s32_384':
         vision_transformer_hybrid_model = create_model('vit_large_r50_s32_384', pretrained=True, num_classes=num_classes)
     elif vision_transformer_hybrid_type == 'vit_small_resnet26d_224':
-        vision_transformer_hybrid_model = create_model('vit_small_resnet26d_224', pretrained=True, num_classes=num_classes)
+        vision_transformer_hybrid_model = create_model('vit_small_resnet26d_224', pretrained=False, num_classes=num_classes)
     elif vision_transformer_hybrid_type == 'vit_small_resnet50d_s16_224':
-        vision_transformer_hybrid_model = create_model('vit_small_resnet50d_s16_224',pretrained=True, num_classes=num_classes)
+        vision_transformer_hybrid_model = create_model('vit_small_resnet50d_s16_224',pretrained=False, num_classes=num_classes)
     elif vision_transformer_hybrid_type == 'vit_base_resnet26d_224':
-        vision_transformer_hybrid_model = create_model('vit_base_resnet26d_224', pretrained=True, num_classes=num_classes)
+        vision_transformer_hybrid_model = create_model('vit_base_resnet26d_224', pretrained=False, num_classes=num_classes)
     elif vision_transformer_hybrid_type == 'vit_base_resnet50d_224':
-        vision_transformer_hybrid_model = create_model('vit_base_resnet50d_224', pretrained=True, num_classes=num_classes)
+        vision_transformer_hybrid_model = create_model('vit_base_resnet50d_224', pretrained=False, num_classes=num_classes)
     else:
         raise ValueError(f'Unknown Vision Transformer Hybrid Architecture: {vision_transformer_hybrid_type}')
 
