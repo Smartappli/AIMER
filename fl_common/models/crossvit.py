c2e2c76926bb8c0ec6387e9cd6ab5f562bb4fe94
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-03-04 22:34:34 +0100

    Tests fix

--- fl_common/models/crossvit.py
+++ fl_common/models/crossvit.py
@@ -27,27 +27,60 @@ def get_crossvit_model(crossvit_type, num_classes):
         ValueError: If the specified Crossvit architecture type is unknown.
     """
     if crossvit_type == "crossvit_tiny_240":
-        crossvit_model = create_model('crossvit_tiny_240', pretrained=True, num_classes=num_classes)
+        try:
+            crossvit_model = create_model('crossvit_tiny_240', pretrained=True, num_classes=num_classes)
+        except:
+            crossvit_model = create_model('crossvit_tiny_240', pretrained=False, num_classes=num_classes)
     elif crossvit_type == "crossvit_small_240":
-        crossvit_model = create_model('crossvit_small_240', pretrained=True, num_classes=num_classes)
+        try:
+            crossvit_model = create_model('crossvit_small_240', pretrained=True, num_classes=num_classes)
+        except:
+            crossvit_model = create_model('crossvit_small_240', pretrained=False, num_classes=num_classes)
     elif crossvit_type == "crossvit_base_240":
-        crossvit_model = create_model('crossvit_base_240', pretrained=True, num_classes=num_classes)
+        try:
+            crossvit_model = create_model('crossvit_base_240', pretrained=True, num_classes=num_classes)
+        except:
+            crossvit_model = create_model('crossvit_base_240', pretrained=False, num_classes=num_classes)
     elif crossvit_type == "crossvit_9_240":
-        crossvit_model = create_model('crossvit_9_240', pretrained=True, num_classes=num_classes)
+        try:
+            crossvit_model = create_model('crossvit_9_240', pretrained=True, num_classes=num_classes)
+        except:
+            crossvit_model = create_model('crossvit_9_240', pretrained=False, num_classes=num_classes)
     elif crossvit_type == "crossvit_15_240":
-        crossvit_model = create_model('crossvit_15_240', pretrained=True, num_classes=num_classes)
+        try:
+            crossvit_model = create_model('crossvit_15_240', pretrained=True, num_classes=num_classes)
+        except:
+            crossvit_model = create_model('crossvit_15_240', pretrained=False, num_classes=num_classes)
     elif crossvit_type == "crossvit_18_240":
-        crossvit_model = create_model('crossvit_18_240', pretrained=True, num_classes=num_classes)
+        try:
+            crossvit_model = create_model('crossvit_18_240', pretrained=True, num_classes=num_classes)
+        except:
+            crossvit_model = create_model('crossvit_18_240', pretrained=False, num_classes=num_classes)
     elif crossvit_type == "crossvit_9_dagger_240":
-        crossvit_model = create_model('crossvit_9_dagger_240', pretrained=True, num_classes=num_classes)
+        try:
+            crossvit_model = create_model('crossvit_9_dagger_240', pretrained=True, num_classes=num_classes)
+        except:
+            crossvit_model = create_model('crossvit_9_dagger_240', pretrained=False, num_classes=num_classes)
     elif crossvit_type == "crossvit_15_dagger_240":
-        crossvit_model = create_model('crossvit_15_dagger_240', pretrained=True, num_classes=num_classes)
+        try:
+            crossvit_model = create_model('crossvit_15_dagger_240', pretrained=True, num_classes=num_classes)
+        except:
+            crossvit_model = create_model('crossvit_15_dagger_240', pretrained=False, num_classes=num_classes)
     elif crossvit_type == "crossvit_15_dagger_408":
-        crossvit_model = create_model('crossvit_15_dagger_408', pretrained=True, num_classes=num_classes)
+        try:
+            crossvit_model = create_model('crossvit_15_dagger_408', pretrained=True, num_classes=num_classes)
+        except:
+            crossvit_model = create_model('crossvit_15_dagger_408', pretrained=False, num_classes=num_classes)
     elif crossvit_type == "crossvit_18_dagger_240":
-        crossvit_model = create_model('crossvit_18_dagger_240', pretrained=True, num_classes=num_classes)
+        try:
+            crossvit_model = create_model('crossvit_18_dagger_240', pretrained=True, num_classes=num_classes)
+        except:
+            crossvit_model = create_model('crossvit_18_dagger_240', pretrained=False, num_classes=num_classes)
     elif crossvit_type == "crossvit_18_dagger_408":
-        crossvit_model = create_model('crossvit_18_dagger_408', pretrained=True, num_classes=num_classes)
+        try:
+            crossvit_model = create_model('crossvit_18_dagger_408', pretrained=True, num_classes=num_classes)
+        except:
+            crossvit_model = create_model('crossvit_18_dagger_408', pretrained=False, num_classes=num_classes)
     else:
         raise ValueError(f'Unknown Crossvit Architecture: {crossvit_type}')
 
