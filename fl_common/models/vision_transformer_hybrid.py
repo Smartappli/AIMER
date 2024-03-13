d75e5e51c93455ad8a14ff3510e841af6e8724d5
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-03-13 09:36:50 +0100

    Lint fixes

--- fl_common/models/vision_transformer_hybrid.py
+++ fl_common/models/vision_transformer_hybrid.py
@@ -2,6 +2,33 @@ from timm import create_model
 
 
 def get_vision_transformer_hybrid_model(vision_transformer_hybrid_type, num_classes):
+    """
+    Retrieves a Vision Transformer Hybrid model based on the specified architecture.
+
+    Args:
+        vision_transformer_hybrid_type (str): Type of Vision Transformer Hybrid architecture.
+            Supported values are:
+                - 'vit_tiny_r_s16_p8_224'
+                - 'vit_tiny_r_s16_p8_384'
+                - 'vit_small_r26_s32_224'
+                - 'vit_small_r26_s32_384'
+                - 'vit_base_r26_s32_224'
+                - 'vit_base_r50_s16_224'
+                - 'vit_base_r50_s16_384'
+                - 'vit_large_r50_s32_224'
+                - 'vit_large_r50_s32_384'
+                - 'vit_small_resnet26d_224'
+                - 'vit_small_resnet50d_s16_224'
+                - 'vit_base_resnet26d_224'
+                - 'vit_base_resnet50d_224'
+        num_classes (int): Number of output classes.
+
+    Returns:
+        torch.nn.Module: Vision Transformer Hybrid model instance.
+
+    Raises:
+        ValueError: If an unsupported vision_transformer_hybrid_type is provided.
+    """
     if vision_transformer_hybrid_type == 'vit_tiny_r_s16_p8_224':
         try:
             vision_transformer_hybrid_model = create_model('vit_tiny_r_s16_p8_224',
