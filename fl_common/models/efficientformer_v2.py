6195ab000503a9f6c73dd8528e8cbd42715f9437
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-05-04 21:17:10 +0200

    Add efficientvit_mit models

--- fl_common/models/efficientformer_v2.py
+++ fl_common/models/efficientformer_v2.py
@@ -2,6 +2,17 @@ from timm import create_model
 
 
 def get_efficientformer_v2_model(efficientformer_v2_type, num_classes):
+    """
+    Get an Efficientformer v2 model of specified type.
+
+    Args:
+        efficientformer_v2_type (str): Type of Efficientformer v2 model.
+            It should be one of ['efficientformerv2_s0', 'efficientformerv2_s1', 'efficientformerv2_s2', 'efficientformerv2_l'].
+        num_classes (int): Number of output classes.
+
+    Returns:
+        torch.nn.Module: Efficientformer v2 model.
+    """
     if efficientformer_v2_type == 'efficientformerv2_s0':
         try:
             efficientformer_v2_model = create_model('efficientformerv2_s0',
