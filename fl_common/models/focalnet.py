1bb21ed22a7c5d03e197239708e0d3ce017614cc
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-05-08 20:57:25 +0200

    refactkring

--- fl_common/models/focalnet.py
+++ fl_common/models/focalnet.py
@@ -14,120 +14,23 @@ def get_focalnet_model(focalnet_type, num_classes):
         num_classes (int): Number of output classes.
 
     Returns:
-        focalnet_model: A Focalnet model instance based on the specified architecture type.
+        torch.nn.Module: A Focalnet model instance based on the specified architecture type.
 
     Raises:
         ValueError: If an unknown Focalnet architecture type is specified.
     """
-    if focalnet_type == 'focalnet_tiny_srf':
-        try:
-            focalnet_model = create_model('focalnet_tiny_srf',
-                                          pretrained=True,
-                                          num_classes=num_classes)
-        except RuntimeError:
-            focalnet_model = create_model('focalnet_tiny_srf',
-                                          pretrained=False,
-                                          num_classes=num_classes)
-    elif focalnet_type == 'focalnet_small_srf':
-        try:
-            focalnet_model = create_model('focalnet_small_srf',
-                                          pretrained=True,
-                                          num_classes=num_classes)
-        except RuntimeError:
-            focalnet_model = create_model('focalnet_small_srf',
-                                          pretrained=False,
-                                          num_classes=num_classes)
-    elif focalnet_type == 'focalnet_base_srf':
-        try:
-            focalnet_model = create_model('focalnet_base_srf',
-                                          pretrained=True,
-                                          num_classes=num_classes)
-        except RuntimeError:
-            focalnet_model = create_model('focalnet_base_srf',
-                                          pretrained=False,
-                                          num_classes=num_classes)
-    elif focalnet_type == 'focalnet_tiny_lrf':
-        try:
-            focalnet_model = create_model('focalnet_tiny_lrf',
-                                          pretrained=True,
-                                          num_classes=num_classes)
-        except RuntimeError:
-            focalnet_model = create_model('focalnet_base_srf',
-                                          pretrained=False,
-                                          num_classes=num_classes)
-    elif focalnet_type == 'focalnet_small_lrf':
-        try:
-            focalnet_model = create_model('focalnet_small_lrf',
-                                          pretrained=True,
-                                          num_classes=num_classes)
-        except RuntimeError:
-            focalnet_model = create_model('focalnet_small_lrf',
-                                          pretrained=False,
-                                          num_classes=num_classes)
-    elif focalnet_type == 'focalnet_base_lrf':
-        try:
-            focalnet_model = create_model('focalnet_base_lrf',
-                                          pretrained=True,
-                                          num_classes=num_classes)
-        except RuntimeError:
-            focalnet_model = create_model('focalnet_base_lrf',
-                                          pretrained=False,
-                                          num_classes=num_classes)
-    elif focalnet_type == 'focalnet_large_fl3':
-        try:
-            focalnet_model = create_model('focalnet_large_fl3',
-                                          pretrained=True,
-                                          num_classes=num_classes)
-        except RuntimeError:
-            focalnet_model = create_model('focalnet_large_fl3',
-                                          pretrained=False,
-                                          num_classes=num_classes)
-    elif focalnet_type == 'focalnet_large_fl4':
-        try:
-            focalnet_model = create_model('focalnet_large_fl4',
-                                          pretrained=True,
-                                          num_classes=num_classes)
-        except RuntimeError:
-            focalnet_model = create_model('focalnet_large_fl4',
-                                          pretrained=False,
-                                          num_classes=num_classes)
-    elif focalnet_type == 'focalnet_xlarge_fl3':
-        try:
-            focalnet_model = create_model('focalnet_xlarge_fl3',
-                                          pretrained=True,
-                                          num_classes=num_classes)
-        except RuntimeError:
-            focalnet_model = create_model('focalnet_xlarge_fl3',
-                                          pretrained=False,
-                                          num_classes=num_classes)
-    elif focalnet_type == 'focalnet_xlarge_fl4':
-        try:
-            focalnet_model = create_model('focalnet_xlarge_fl4',
-                                          pretrained=True,
-                                          num_classes=num_classes)
-        except RuntimeError:
-            focalnet_model = create_model('focalnet_xlarge_fl4',
-                                          pretrained=False,
-                                          num_classes=num_classes)
-    elif focalnet_type == 'focalnet_huge_fl3':
-        try:
-            focalnet_model = create_model('focalnet_huge_fl3',
-                                          pretrained=True,
-                                          num_classes=num_classes)
-        except RuntimeError:
-            focalnet_model = create_model('focalnet_huge_fl3',
-                                          pretrained=False,
-                                          num_classes=num_classes)
-    elif focalnet_type == 'focalnet_huge_fl4':
-        try:
-            focalnet_model = create_model('focalnet_huge_fl4',
-                                          pretrained=True,
-                                          num_classes=num_classes)
-        except RuntimeError:
-            focalnet_model = create_model('focalnet_huge_fl4',
-                                          pretrained=False,
-                                          num_classes=num_classes)
-    else:
+    supported_types = {
+        'focalnet_tiny_srf', 'focalnet_small_srf', 'focalnet_base_srf',
+        'focalnet_tiny_lrf', 'focalnet_small_lrf', 'focalnet_base_lrf',
+        'focalnet_large_fl3', 'focalnet_large_fl4', 'focalnet_xlarge_fl3',
+        'focalnet_xlarge_fl4', 'focalnet_huge_fl3', 'focalnet_huge_fl4'
+    }
+
+    if focalnet_type not in supported_types:
         raise ValueError(f'Unknown Focalnet Architecture: {focalnet_type}')
 
-    return focalnet_model
+    try:
+        return create_model(focalnet_type, pretrained=True, num_classes=num_classes)
+    except RuntimeError as e:
+        print(f"Error loading pretrained model: {e}")
+        return create_model(focalnet_type, pretrained=False, num_classes=num_classes)
