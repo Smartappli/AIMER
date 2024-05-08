10f745a3d7ec49a9d99dbae2e8a7acaa225d02b2
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-05-08 07:28:16 +0200

    refactkring

--- fl_common/models/hrnet.py
+++ fl_common/models/hrnet.py
@@ -6,18 +6,8 @@ def get_hrnet_model(hrnet_type, num_classes):
     Get an HRNet model.
 
     Parameters:
-        hrnet_type (str): Type of HRNet architecture. Options include:
-            - "hrnet_w18_small"
-            - "hrnet_w18_small_v2"
-            - "hrnet_w18"
-            - "hrnet_w30"
-            - "hrnet_w32"
-            - "hrnet_w40"
-            - "hrnet_w44"
-            - "hrnet_w48"
-            - "hrnet_w64"
-            - "hrnet_w18_ssld"
-            - "hrnet_w48_ssld"
+        hrnet_type (str): Type of HRNet architecture. Options include 'hrnet_w18_small', 'hrnet_w18_small_v2',
+                          'hrnet_w18' to 'hrnet_w64', and 'hrnet_w18_ssld' to 'hrnet_w48_ssld'.
         num_classes (int): Number of output classes.
 
     Returns:
@@ -26,106 +16,17 @@ def get_hrnet_model(hrnet_type, num_classes):
     Raises:
         ValueError: If an unknown HRNet architecture is specified.
     """
-    if hrnet_type == 'hrnet_w18_small':
-        try:
-            hrnet_model = create_model('hrnet_w18_small',
-                                       pretrained=True,
-                                       num_classes=num_classes)
-        except Exception:
-            hrnet_model = create_model('hrnet_w18_small',
-                                       pretrained=False,
-                                       num_classes=num_classes)
-    elif hrnet_type == 'hrnet_w18_small_v2':
-        try:
-            hrnet_model = create_model('hrnet_w18_small_v2',
-                                       pretrained=True,
-                                       num_classes=num_classes)
-        except Exception:
-            hrnet_model = create_model('hrnet_w18_small_v2',
-                                       pretrained=False,
-                                       num_classes=num_classes)
-    elif hrnet_type == 'hrnet_w18':
-        try:
-            hrnet_model = create_model('hrnet_w18',
-                                       pretrained=True,
-                                       num_classes=num_classes)
-        except Exception:
-            hrnet_model = create_model('hrnet_w18',
-                                       pretrained=False,
-                                       num_classes=num_classes)
-    elif hrnet_type == 'hrnet_w30':
-        try:
-            hrnet_model = create_model('hrnet_w30',
-                                       pretrained=True,
-                                       num_classes=num_classes)
-        except Exception:
-            hrnet_model = create_model('hrnet_w30',
-                                       pretrained=False,
-                                       num_classes=num_classes)
-    elif hrnet_type == 'hrnet_w32':
-        try:
-            hrnet_model = create_model('hrnet_w32',
-                                       pretrained=True,
-                                       num_classes=num_classes)
-        except Exception:
-            hrnet_model = create_model('hrnet_w32',
-                                       pretrained=False,
-                                       num_classes=num_classes)
-    elif hrnet_type == 'hrnet_w40':
-        try:
-            hrnet_model = create_model('hrnet_w40',
-                                       pretrained=True,
-                                       num_classes=num_classes)
-        except Exception:
-            hrnet_model = create_model('hrnet_w40',
-                                       pretrained=False,
-                                       num_classes=num_classes)
-    elif hrnet_type == 'hrnet_w44':
-        try:
-            hrnet_model = create_model('hrnet_w44',
-                                       pretrained=True,
-                                       num_classes=num_classes)
-        except Exception:
-            hrnet_model = create_model('hrnet_w44',
-                                       pretrained=False,
-                                       num_classes=num_classes)
-    elif hrnet_type == 'hrnet_w48':
-        try:
-            hrnet_model = create_model('hrnet_w48',
-                                       pretrained=True,
-                                       num_classes=num_classes)
-        except Exception:
-            hrnet_model = create_model('hrnet_w48',
-                                       pretrained=False,
-                                       num_classes=num_classes)
-    elif hrnet_type == 'hrnet_w64':
-        try:
-            hrnet_model = create_model('hrnet_w64',
-                                       pretrained=True,
-                                       num_classes=num_classes)
-        except Exception:
-            hrnet_model = create_model('hrnet_w64',
-                                       pretrained=False,
-                                       num_classes=num_classes)
-    elif hrnet_type == 'hrnet_w18_ssld':
-        try:
-            hrnet_model = create_model('hrnet_w18_ssld',
-                                       pretrained=True,
-                                       num_classes=num_classes)
-        except Exception:
-            hrnet_model = create_model('hrnet_w18_ssld',
-                                       pretrained=False,
-                                       num_classes=num_classes)
-    elif hrnet_type == 'hrnet_w48_ssld':
-        try:
-            hrnet_model = create_model('hrnet_w48_ssld',
-                                       pretrained=True,
-                                       num_classes=num_classes)
-        except Exception:
-            hrnet_model = create_model('hrnet_w48_ssld',
-                                       pretrained=False,
-                                       num_classes=num_classes)
-    else:
-        raise ValueError(f'Unknown Hrnet Architecture: {hrnet_type}')
+    hrnet_options = ['hrnet_w18_small', 'hrnet_w18_small_v2', 'hrnet_w18', 'hrnet_w30', 'hrnet_w32',
+                     'hrnet_w40', 'hrnet_w44', 'hrnet_w48', 'hrnet_w64', 'hrnet_w18_ssld', 'hrnet_w48_ssld']
 
-    return hrnet_model
+    if hrnet_type not in hrnet_options:
+        raise ValueError(f'Unknown HRNet Architecture: {hrnet_type}')
+
+    try:
+        return create_model(hrnet_type, pretrained=True, num_classes=num_classes)
+    except OSError as e:
+        print(f"Error loading pretrained model: {e}")
+        return create_model(hrnet_type, pretrained=False, num_classes=num_classes)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
