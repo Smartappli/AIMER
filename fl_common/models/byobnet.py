35679ca694f4f4261be1aa54cb0767d52b685e14
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-09 23:30:56 +0200

    Update byobnet.py

--- fl_common/models/byobnet.py
+++ fl_common/models/byobnet.py
@@ -23,7 +23,7 @@ def get_byobnet_model(byobnet_type, num_classes):
         'seresnet33ts', 'eca_resnet33ts', 'gcresnet50t', 'gcresnext50ts', 'regnetz_b16',
         'regnetz_c16', 'regnetz_d32', 'regnetz_d8', 'regnetz_e8', 'regnetz_b16_evos',
         'regnetz_c16_evos', 'regnetz_d8_evos', 'mobileone_s0', 'mobileone_s1', 'mobileone_s2',
-        'mobileone_s3',"mobileone_s4"
+        'mobileone_s3', "mobileone_s4"
     ]
 
     if byobnet_type not in model_types:
