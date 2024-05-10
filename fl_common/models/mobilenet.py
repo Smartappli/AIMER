08160d7126c460c0ec4e099132314ab477f5fcb2
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 14:02:28 +0200

    Update mobilenet.py

--- fl_common/models/mobilenet.py
+++ fl_common/models/mobilenet.py
@@ -33,215 +33,43 @@ def get_mobilenet_model(mobilenet_type, num_classes):
     model = get_mobilenet_model(mobilenet_type='MobileNet_V2', num_classes=10)
     ```
     """
-    torch_vision = False
-    # Load the pre-trained version of VGG
-    if mobilenet_type == 'MobileNet_V2':
-        torch_vision = True
-        try:
-            weights = models.MobileNet_V2_Weights.DEFAULT
-            mobilenet_model = models.mobilenet_v2(weights=weights)
-        except RuntimeError:
-            mobilenet_model = models.mobilenet_v2(weights=None)
-    elif mobilenet_type == 'MobileNet_V3_Small':
-        torch_vision = True
-        try:
-            weights = models.MobileNet_V3_Small_Weights.DEFAULT
-            mobilenet_model = models.mobilenet_v3_small(weights=weights)
-        except RuntimeError:
-            mobilenet_model = models.mobilenet_v3_small(weights=None)
-    elif mobilenet_type == 'MobileNet_V3_Large':
-        torch_vision = True
-        try:
-            weights = models.MobileNet_V3_Large_Weights.DEFAULT
-            mobilenet_model = models.mobilenet_v3_large(weights=weights)
-        except RuntimeError:
-            mobilenet_model = models.mobilenet_v3_large(weights=None)
-    elif mobilenet_type == 'mobilenetv3_large_075':
-        try:
-            mobilenet_model = create_model('mobilenetv3_large_075',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('mobilenetv3_large_075',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'mobilenetv3_large_100':
-        try:
-            mobilenet_model = create_model('mobilenetv3_large_100',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('mobilenetv3_large_100',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'mobilenetv3_small_050':
-        try:
-            mobilenet_model = create_model('mobilenetv3_small_050',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('mobilenetv3_small_050',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'mobilenetv3_small_075':
-        try:
-            mobilenet_model = create_model('mobilenetv3_small_075',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('mobilenetv3_small_075',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'mobilenetv3_small_100':
-        try:
-            mobilenet_model = create_model('mobilenetv3_small_100',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('mobilenetv3_small_100',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'mobilenetv3_rw':
-        try:
-            mobilenet_model = create_model('mobilenetv3_rw',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('mobilenetv3_rw',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'tf_mobilenetv3_large_075':
-        try:
-            mobilenet_model = create_model('tf_mobilenetv3_large_075',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('tf_mobilenetv3_large_075',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'tf_mobilenetv3_large_100':
-        try:
-            mobilenet_model = create_model('tf_mobilenetv3_large_100',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('tf_mobilenetv3_large_100',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'tf_mobilenetv3_large_minimal_100':
-        try:
-            mobilenet_model = create_model('tf_mobilenetv3_large_minimal_100',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('tf_mobilenetv3_large_minimal_100',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'tf_mobilenetv3_small_075':
-        try:
-            mobilenet_model = create_model('tf_mobilenetv3_small_075',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('tf_mobilenetv3_small_075',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'tf_mobilenetv3_small_100':
-        try:
-            mobilenet_model = create_model('tf_mobilenetv3_small_100',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('tf_mobilenetv3_small_100',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'tf_mobilenetv3_small_minimal_100':
-        try:
-            mobilenet_model = create_model('tf_mobilenetv3_small_minimal_100',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('tf_mobilenetv3_small_minimal_100',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'fbnetv3_b':
-        try:
-            mobilenet_model = create_model('fbnetv3_b',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('fbnetv3_b',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'fbnetv3_d':
-        try:
-            mobilenet_model = create_model('fbnetv3_d',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('fbnetv3_d',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'fbnetv3_g':
-        try:
-            mobilenet_model = create_model('fbnetv3_g',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('fbnetv3_g',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'lcnet_035':
-        try:
-            mobilenet_model = create_model('lcnet_035',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('lcnet_035',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'lcnet_050':
-        try:
-            mobilenet_model = create_model('lcnet_050',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('lcnet_050',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'lcnet_075':
-        try:
-            mobilenet_model = create_model('lcnet_075',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('lcnet_075',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'lcnet_100':
-        try:
-            mobilenet_model = create_model('lcnet_100',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('lcnet_100',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    elif mobilenet_type == 'lcnet_150':
-        try:
-            mobilenet_model = create_model('lcnet_150',
-                                           pretrained=True,
-                                           num_classes=num_classes)
-        except RuntimeError:
-            mobilenet_model = create_model('lcnet_150',
-                                           pretrained=False,
-                                           num_classes=num_classes)
-    else:
-        raise ValueError(f'Unknown MobileNet Architecture : {mobilenet_type}')
+    # Mapping of vision types to their corresponding torchvision models and weights
+    torchvision_models = {
+        'MobileNet_V2': (models.mobilenet_v2, models.MobileNet_V2_Weights),
+        'MobileNet_V3_Small': (models.mobilenet_v3_small, models.MobileNet_V3_Small_Weights),
+        'MobileNet_V3_Large': (models.mobilenet_v3_large, models.MobileNet_V3_Large_Weights)
+    }
 
-    if torch_vision:
+    timm_models = [
+        "mobilenetv3_large_075", "'mobilenetv3_large_100", "mobilenetv3_small_050", "mobilenetv3_small_075",
+        "mobilenetv3_small_075", "mobilenetv3_small_100", "mobilenetv3_rw", "tf_mobilenetv3_large_075",
+        "tf_mobilenetv3_large_100", "tf_mobilenetv3_large_minimal_100", "tf_mobilenetv3_small_075",
+        "tf_mobilenetv3_small_100", "tf_mobilenetv3_small_minimal_100", "fbnetv3_b", "fbnetv3_d",
+        "fbnetv3_g", "lcnet_035", "lcnet_050", "lcnet_075", "lcnet_100", "lcnet_150"
+    ]
+
+    # Check if the vision type is from torchvision
+    if mobilenet_type in torchvision_models:
+        model_func, weights_class = torchvision_models[mobilenet_type]
+        try:
+            weights = weights_class.DEFAULT
+            mobilenet_model = model_func(weights=weights)
+        except RuntimeError as e:
+            print(f"{mobilenet_type} - Error loading pretrained model: {e}")
+            mobilenet_model = model_func(weights=None)
+
         # Modify last layer to suit number of classes
         num_features = mobilenet_model.classifier[-1].in_features
         mobilenet_model.classifier[-1] = nn.Linear(num_features, num_classes)
+
+    # Check if the vision type is from the 'timm' library
+    elif mobilenet_type in timm_models:
+        try:
+            mobilenet_model = create_model(mobilenet_type, pretrained=True, num_classes=num_classes)
+        except RuntimeError as e:
+            print(f"{mobilenet_type} - Error loading pretrained model: {e}")
+            mobilenet_model = create_model(mobilenet_type, pretrained=False, num_classes=num_classes)
+    else:
+        raise ValueError(f'Unknown MobileNet Architecture : {mobilenet_type}')
 
     return mobilenet_model
