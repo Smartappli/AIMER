f1368d1ce97183bce69ed8aca0b38a8338b3171b
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 22:36:24 +0200

    Update volo.py

--- fl_common/models/volo.py
+++ fl_common/models/volo.py
@@ -27,5 +27,5 @@ def get_volo_model(volo_type, num_classes):
     try:
         return create_model(volo_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{volo_type} - Error loading pretrained model: {e}")
         return create_model(volo_type, pretrained=False, num_classes=num_classes)
