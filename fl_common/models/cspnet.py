d18925e2ba314704f19ee974d160f883898d2145
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 21:12:14 +0200

    Update cspnet.py

--- fl_common/models/cspnet.py
+++ fl_common/models/cspnet.py
@@ -31,5 +31,5 @@ def get_cspnet_model(cspnet_type, num_classes):
     try:
         return create_model(cspnet_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{cspnet_type} - Error loading pretrained model: {e}")
         return create_model(cspnet_type, pretrained=False, num_classes=num_classes)
