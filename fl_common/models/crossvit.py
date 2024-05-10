34c8fc6995ccb6e11df248219400ee9598c742fe
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 21:11:55 +0200

    Update crossvit.py

--- fl_common/models/crossvit.py
+++ fl_common/models/crossvit.py
@@ -29,5 +29,5 @@ def get_crossvit_model(crossvit_type, num_classes):
     try:
         return create_model(crossvit_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{crossvit_type} - Error loading pretrained model: {e}")
         return create_model(crossvit_type, pretrained=False, num_classes=num_classes)
