e85f3499f7620e47ff631a3360be55a8d56b2e6b
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 22:38:51 +0200

    Update visformer.py

--- fl_common/models/visformer.py
+++ fl_common/models/visformer.py
@@ -22,5 +22,5 @@ def get_visformer_model(visformer_type, num_classes):
     try:
         return create_model(visformer_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{visformer_type} - Error loading pretrained model: {e}")
         return create_model(visformer_type, pretrained=False, num_classes=num_classes)
