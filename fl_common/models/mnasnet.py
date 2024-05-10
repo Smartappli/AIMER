43fed622019cdbf53cb6fe33cd5f3e4e7f1cad24
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 21:03:29 +0200

    Update mnasnet.py
    refactoring

--- fl_common/models/mnasnet.py
+++ fl_common/models/mnasnet.py
@@ -2,55 +2,31 @@ import torch.nn as nn
 from torchvision import models
 
 
-def get_mnasnet_model(mnasnet_type, num_classes):
+def get_mnasnet_model(mnasnet_type: str, num_classes: int) -> nn.Module:
     """
     Load a pre-trained MNASNet model of the specified type and modify its
     last layer to accommodate the given number of classes.
-
-    Parameters:
-    - mnasnet_type (str): Type of MNASNet architecture, supported types:
-        - 'MNASNet0_5'
-        - 'MNASNet0_75'
-        - 'MNASNet1_0'
-        - 'MNASNet1_3'
-    - num_classes (int): Number of output classes for the modified last layer.
-
-    Returns:
-    - torch.nn.Module: Modified MNASNet model with the specified architecture
-      and last layer adapted for the given number of classes.
-
-    Raises:
-    - ValueError: If an unknown MNASNet architecture type is provided.
     """
-    # Load the pre-trained version of MNASNet based on the specified type
-    if mnasnet_type == 'MNASNet0_5':
-        try:
-            weights = models.MNASNet0_5_Weights.DEFAULT
-            mnasnet_model = models.mnasnet.mnasnet0_5(weights=weights)
-        except RuntimeError:
-            mnasnet_model = models.mnasnet.mnasnet0_5(weights=None)
-    elif mnasnet_type == 'MNASNet0_75':
-        try:
-            weights = models.MNASNet0_75_Weights.DEFAULT
-            mnasnet_model = models.mnasnet.mnasnet0_75(weights=weights)
-        except RuntimeError:
-            mnasnet_model = models.mnasnet.mnasnet0_75(weights=None)
-    elif mnasnet_type == 'MNASNet1_0':
-        try:
-            weights = models.MNASNet1_0_Weights.DEFAULT
-            mnasnet_model = models.mnasnet.mnasnet1_0(weights=weights)
-        except RuntimeError:
-            mnasnet_model = models.mnasnet.mnasnet1_0(weights=None)
-    elif mnasnet_type == 'MNASNet1_3':
-        try:
-            weights = models.MNASNet1_3_Weights.DEFAULT
-            mnasnet_model = models.mnasnet.mnasnet1_3(weights=weights)
-        except RuntimeError:
-            mnasnet_model = models.mnasnet.mnasnet1_3(weights=None)
-    else:
+    # Mapping of MNASNet types to their corresponding weight classes
+    weight_classes = {
+        'MNASNet0_5': models.MNASNet0_5_Weights,
+        'MNASNet0_75': models.MNASNet0_75_Weights,
+        'MNASNet1_0': models.MNASNet1_0_Weights,
+        'MNASNet1_3': models.MNASNet1_3_Weights
+    }
+
+    # Check if the specified type is valid
+    if mnasnet_type not in weight_classes:
         raise ValueError(f'Unknown MNASNet Architecture: {mnasnet_type}')
 
-    # Modify the last layer to suit the given number of classes
+    # Load the pre-trained model
+    try:
+        weights = weight_classes[mnasnet_type].DEFAULT
+        mnasnet_model = getattr(models.mnasnet, mnasnet_type.lower())(weights=weights)
+    except RuntimeError:
+        mnasnet_model = getattr(models.mnasnet, mnasnet_type.lower())(weights=None)
+
+    # Modify the last layer
     num_features = mnasnet_model.classifier[1].in_features
     mnasnet_model.classifier[1] = nn.Linear(num_features, num_classes)
 
