9c56acaa9c32c1d5b2d1cd503785c978a1f586e7
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 16:27:33 +0200

    Update convnext.py

--- fl_common/models/convnext.py
+++ fl_common/models/convnext.py
@@ -18,23 +18,20 @@ def get_convnext_model(convnext_type, num_classes):
     Raises:
     - ValueError: If the provided convnext_type is not recognized or the model structure is not as expected.
     """
-    torch_vision = False
     # Mapping of vision types to their corresponding torchvision models and weights
     torchvision_models = {
         'ConvNeXt_Tiny': (models.convnext_tiny, models.ConvNeXt_Tiny_Weights.DEFAULTs),
         'ConvNeXt_Small': (models.convnext_small, models.ConvNeXt_Small_Weights.DEFAULT),
         'ConvNeXt_Base': (models.convnext_base, models.ConvNeXt_Base_Weights.DEFAULT),
-        'ConvNeXt_Large': ( models.convnext_large, models.ConvNeXt_Large_Weights.DEFAULT)
+        'ConvNeXt_Large': (models.convnext_large, models.ConvNeXt_Large_Weights.DEFAULT)
     }
-
 
     timm_models = [
         "convnext_atto", "convnext_atto_ols", "convnext_femto", "convnext_femto_ols", "convnext_pico",
         "convnext_pico_ols", "convnext_nano", "convnext_nano_ols", "convnext_tiny_hnf", "convnext_tiny",
         "convnext_small", "convnext_base", "convnext_large", "convnext_large_mlp", "convnext_xlarge",
         "convnext_xxlarge", "convnextv2_atto", "convnextv2_femto", "convnextv2_pico", "convnextv2_nano",
-        "convnextv2_small", "convnextv2_tiny", "convnextv2_base", "convnextv2_large", "convnextv2_huge",
-        
+        "convnextv2_small", "convnextv2_tiny", "convnextv2_base", "convnextv2_large", "convnextv2_huge" 
     ]
 
     # Check if the vision type is from torchvision
