57cd1f783c7899b618b19ec6ac9114a636aa2101
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 21:18:04 +0200

    Update efficientvit_mit.py

--- fl_common/models/efficientvit_mit.py
+++ fl_common/models/efficientvit_mit.py
@@ -28,5 +28,5 @@ def get_efficientvit_mit_model(efficientvit_mit_type, num_classes):
     try:
         return create_model(efficientvit_mit_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{efficientvit_mit_type} - Error loading pretrained model: {e}")
         return create_model(efficientvit_mit_type, pretrained=False, num_classes=num_classes)
