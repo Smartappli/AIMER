f5044fd1517bda0bbb79c4b7162461ac13924dff
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 21:19:51 +0200

    Update fastvit.py

--- fl_common/models/fastvit.py
+++ fl_common/models/fastvit.py
@@ -26,5 +26,5 @@ def get_fastvit_model(fastvit_type, num_classes):
     try:
         return create_model(fastvit_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{fastvit_type} - Error loading pretrained model: {e}")
         return create_model(fastvit_type, pretrained=False, num_classes=num_classes)
