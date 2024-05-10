f1f1aec7255cd73721deb16b5f91bf1cab784469
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 23:40:22 +0200

    Update senet.py

--- fl_common/models/senet.py
+++ fl_common/models/senet.py
@@ -27,5 +27,5 @@ def get_senet_model(senet_type, num_classes):
     try:
         return create_model(senet_type, pretrained=False, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{senet_type} - Error loading pretrained model: {e}")
         return create_model(senet_type, pretrained=False, num_classes=num_classes)
