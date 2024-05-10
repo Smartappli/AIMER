d7edd63118c1c412575337a3732ae3a7fd20f450
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 23:40:00 +0200

    Update selecsls.py

--- fl_common/models/selecsls.py
+++ fl_common/models/selecsls.py
@@ -25,5 +25,5 @@ def get_selecsls_model(selecsls_type, num_classes):
     try:
         return create_model(selecsls_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{selecsls_type} - Error loading pretrained model: {e}")
         return create_model(selecsls_type, pretrained=False, num_classes=num_classes)
