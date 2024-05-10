52fc982b13f588f26ea938c062ab8b6a23b7a73e
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 21:21:32 +0200

    Update gcvit.py

--- fl_common/models/gcvit.py
+++ fl_common/models/gcvit.py
@@ -27,5 +27,5 @@ def get_gcvit_model(gcvit_type, num_classes):
     try:
         return create_model(gcvit_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{gcvit_type} - Error loading pretrained model: {e}")
         return create_model(gcvit_type, pretrained=False, num_classes=num_classes)
