c10e14bad11bf389521d170a462a57c3913244b6
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 21:12:57 +0200

    Update deit.py

--- fl_common/models/deit.py
+++ fl_common/models/deit.py
@@ -30,5 +30,5 @@ def get_deit_model(deit_type, num_classes):
     try:
         return create_model(deit_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{deit_type} - Error loading pretrained model: {e}")
         return create_model(deit_type, pretrained=False, num_classes=num_classes)
