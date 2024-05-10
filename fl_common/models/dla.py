11206bc573e5de632d28bcb2d6da64ccf0b20ce3
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 21:13:24 +0200

    Update dla.py

--- fl_common/models/dla.py
+++ fl_common/models/dla.py
@@ -27,5 +27,5 @@ def get_dla_model(dla_type, num_classes):
     try:
         return create_model(dla_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{dla_type} - Error loading pretrained model: {e}")
         return create_model(dla_type, pretrained=False, num_classes=num_classes)
