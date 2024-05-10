0d877d139f2ed4742fa98ef69e569150341bc52c
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 22:31:13 +0200

    Update resnest.py

--- fl_common/models/resnest.py
+++ fl_common/models/resnest.py
@@ -26,5 +26,5 @@ def get_resnest_model(resnest_type, num_classes):
     try:
         return create_model(resnest_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{resnest_type} - Error loading pretrained model: {e}")
         return create_model(resnest_type, pretrained=False, num_classes=num_classes)
