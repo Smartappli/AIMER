acf3b1bac17c5bf0e896779ba32f5e1ee24209c4
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 21:14:31 +0200

    Update efficientformer.py

--- fl_common/models/efficientformer.py
+++ fl_common/models/efficientformer.py
@@ -23,5 +23,5 @@ def get_efficientformer_model(efficientformer_type, num_classes):
     try:
         return create_model(efficientformer_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{efficientformer_type} - Error loading pretrained model: {e}")
         return create_model(efficientformer_type, pretrained=False, num_classes=num_classes)
