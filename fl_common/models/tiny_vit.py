3be0a2b990c67f0e497ed40b9091f74db4dd51a8
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 23:51:10 +0200

    Update tiny_vit.py

--- fl_common/models/tiny_vit.py
+++ fl_common/models/tiny_vit.py
@@ -28,5 +28,5 @@ def get_tiny_vit_model(tiny_vit_type, num_classes):
     try:
         return create_model(tiny_vit_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{tiny_vit_type} - Error loading pretrained model: {e}")
         return create_model(tiny_vit_type, pretrained=False, num_classes=num_classes)
