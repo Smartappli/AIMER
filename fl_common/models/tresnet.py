4e8b07eb39f8cdf4865202382c9820c264cbc73e
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 23:52:55 +0200

    Update tresnet.py

--- fl_common/models/tresnet.py
+++ fl_common/models/tresnet.py
@@ -22,5 +22,5 @@ def get_tresnet_model(tresnet_type, num_classes):
     try:
         return create_model(tresnet_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{tresnet_type} - Error loading pretrained model: {e}")
         return create_model(tresnet_type, pretrained=False, num_classes=num_classes)
