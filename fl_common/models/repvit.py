16e08e94f1ed0a0b2391ce09fbf44568f93554b3
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 22:30:33 +0200

    Update repvit.py

--- fl_common/models/repvit.py
+++ fl_common/models/repvit.py
@@ -26,5 +26,5 @@ def get_repvit_model(repvit_type, num_classes):
     try:
         return create_model(repvit_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{repvit_type} - Error loading pretrained model: {e}")
         return create_model(repvit_type, pretrained=False, num_classes=num_classes)
