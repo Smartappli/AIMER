98552e73bf65f1fd8afb20354bf0335cc9ebef57
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 21:50:06 +0200

    Update nfnet.py

--- fl_common/models/nfnet.py
+++ fl_common/models/nfnet.py
@@ -33,5 +33,5 @@ def get_nfnet_model(nfnet_type, num_classes):
     try:
         return create_model(nfnet_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{nfnet_type} - Error loading pretrained model: {e}")
         return create_model(nfnet_type, pretrained=False, num_classes=num_classes)
