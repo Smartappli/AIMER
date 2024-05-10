5966ae1a9d1fecb2c340eb2901b87141dcaf7258
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-10 21:26:41 +0200

    Update nextvit.py

--- fl_common/models/nextvit.py
+++ fl_common/models/nextvit.py
@@ -23,5 +23,5 @@ def get_nextvit_model(nextvit_type, num_classes):
     try:
         return create_model(nextvit_type, pretrained=True, num_classes=num_classes)
     except RuntimeError as e:
-        print(f"Error loading pretrained model: {e}")
+        print(f"{nextvit_type} - Error loading pretrained model: {e}")
         return create_model(nextvit_type, pretrained=False, num_classes=num_classes)
