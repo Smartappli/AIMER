33053f8fca6dc94da3130ae581125f9fb803058f
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-11 11:13:43 +0200

    Update regnet.py

--- fl_common/models/regnet.py
+++ fl_common/models/regnet.py
@@ -38,7 +38,7 @@ def get_regnet_model(regnet_type, num_classes):
         "regnetx_002", "regnetx_004", "regnetx_004_tv", "regnetx_006", "regnetx_008",
         "regnetx_016", "regnetx_032", "regnetx_040", "regnetx_064", "regnetx_080",
         "regnetx_120", "regnetx_160", "regnetx_320", "regnety_002", "regnety_004",
-        "regnety_006", "regnety_008", "'regnety_008_tv", "regnety_016", "regnety_032",
+        "regnety_006", "regnety_008", "regnety_008_tv", "regnety_016", "regnety_032",
         "regnety_040", "regnety_064", "regnety_080", "regnety_080_tv", "regnety_120",
         "regnety_160", "regnety_320", "regnety_640", "regnety_1280", "regnety_2560",
         "regnety_040_sgn", "regnetv_040", "regnetv_064", "regnetz_005", "regnetz_040",
