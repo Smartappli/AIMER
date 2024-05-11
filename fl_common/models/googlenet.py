3b835cb8c5945cc2f75d2cf8ff77e167427fd134
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-11 06:53:01 +0200

    Update googlenet.py

--- fl_common/models/googlenet.py
+++ fl_common/models/googlenet.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 from torchvision import models
 
+
 def get_googlenet_model(googlenet_type, num_classes):
     """
     Load a pre-trained GoogLeNet model of the specified type and modify its
