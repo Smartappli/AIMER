ffc0d1a7dfb2686d1aa8361dc40842a7a121d18d
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-24 23:01:24 +0200

    Update efficientformer_v2.py

--- fl_common/models/efficientformer_v2.py
+++ fl_common/models/efficientformer_v2.py
@@ -20,16 +20,15 @@ def get_efficientformer_v2_model(efficientformer_v2_type, num_classes):
         "efficientformerv2_l",
     ]
     if efficientformer_v2_type not in supported_types:
-        raise ValueError(
-            f"Unknown Efficientformer v2 Architecture: {efficientformer_v2_type}"
-        )
+        msg = f"Unknown Efficientformer v2 Architecture: {efficientformer_v2_type}"
+        raise ValueError(msg)
 
     try:
         return create_model(
-            efficientformer_v2_type, pretrained=True, num_classes=num_classes
+            efficientformer_v2_type, pretrained=True, num_classes=num_classes,
         )
     except RuntimeError as e:
         print(f"{efficientformer_v2_type} - Error loading pretrained model: {e}")
         return create_model(
-            efficientformer_v2_type, pretrained=False, num_classes=num_classes
+            efficientformer_v2_type, pretrained=False, num_classes=num_classes,
         )
