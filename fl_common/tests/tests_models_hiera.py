5d154558ed060bb3cdae07c6ba9b55e383f906c8
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-25 14:17:49 +0200

    Update tests_models_hiera.py

--- fl_common/tests/tests_models_hiera.py
+++ fl_common/tests/tests_models_hiera.py
@@ -38,7 +38,7 @@ class ProcessingHieraTestCase(TestCase):
                     self.assertIsNotNone(model)
                 except ValueError:
                     self.fail(
-                        f"{hiera_type} should be a known hiera architecture."
+                        f"{hiera_type} should be a known hiera architecture.",
                     )
 
     def test_unknown_hiera_type(self):
