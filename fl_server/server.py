48929ca443ad324284a785e23816ff9e34c1b485
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-05-25 16:07:56 +0200

    Update server.py

--- fl_server/server.py
+++ fl_server/server.py
@@ -20,7 +20,7 @@ def launch_node():
 
     print("\n--- Démarrage du noeud Humani ---")
     node_humani = sy.Orchestra.launch(
-        name="do-humani", port=9000, local_db=True, dev_mode=True, reset=True
+        name="do-humani", port=9000, local_db=True, dev_mode=True, reset=True,
     )
     root_domain_humani_client = node_humani.login(
         email="info@openmined.org", password="changethis"
@@ -36,10 +36,10 @@ def launch_node():
 
     print("\n--- Démarrage du noeud Epicura ---")
     node_epicura = sy.Orchestra.launch(
-        name="do-epicura", port=9001, local_db=True, dev_mode=True, reset=True
+        name="do-epicura", port=9001, local_db=True, dev_mode=True, reset=True,
     )
     root_domain_epicura_client = node_epicura.login(
-        email="info@openmined.org", password="changethis"
+        email="info@openmined.org", password="changethis",
     )
     root_domain_epicura_client.register(
         name="Jane Doe",
@@ -52,10 +52,10 @@ def launch_node():
 
     print("\n--- Démarrage du noeud Vivalia ---")
     node_vivalia = sy.Orchestra.launch(
-        name="do-vivalia", port=9003, local_db=True, dev_mode=True, reset=True
+        name="do-vivalia", port=9003, local_db=True, dev_mode=True, reset=True,
     )
     root_domain_vivalia_client = node_vivalia.login(
-        email="info@openmined.org", password="changethis"
+        email="info@openmined.org", password="changethis",
     )
     root_domain_vivalia_client.register(
         name="Jane Doe",
@@ -67,7 +67,7 @@ def launch_node():
     )
 
     ds_client = node_humani.login(
-        email="janedoe@caltech.edu", password="abc123"
+        email="janedoe@caltech.edu", password="abc123",
     )
 
     data_subjects = root_domain_humani_client.data_subject_registry.get_all()
@@ -83,16 +83,16 @@ def launch_node():
                     {
                         "Patient_ID": ["011", "015", "022", "034", "044"],
                         "Age": [40, 39, 35, 60, 25],
-                    }
+                    },
                 ),
                 mock=pd.DataFrame(
                     {
                         "Patient_ID": ["1", "2", "3", "4", "5"],
                         "Age": [50, 49, 45, 70, 35],
-                    }
+                    },
                 ),
                 mock_is_real=False,
-            )
+            ),
         ],
     )
     root_domain_humani_client.upload_dataset(dataset)
