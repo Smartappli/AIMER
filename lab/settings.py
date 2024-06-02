b1ed1a2956289177ac9be7da9134c93232d793cf
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-03 00:23:39 +0200

    Update settings.py

--- lab/settings.py
+++ lab/settings.py
@@ -1,15 +1,17 @@
+import os
 from pathlib import Path
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-config = dotenv_values(".env")
+# Load environment variables from the .env file (if present)
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config["ENV_SECRET_KEY"]
+SECRET_KEY = os.getenv('ENV_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
