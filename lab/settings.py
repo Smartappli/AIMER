cfa70155155c25b56743a2994ddce8f7bfde407b
Smartappli <26173534+Smartappli@users.noreply.github.com>
2024-06-02 22:24:09 +0000

    :art: Format Python code with psf/black

--- lab/settings.py
+++ lab/settings.py
@@ -11,7 +11,7 @@ load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('ENV_SECRET_KEY')
+SECRET_KEY = os.getenv("ENV_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
