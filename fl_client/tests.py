59c71ede2a83b36dbaeecd6b90c1e9e2d5c35132
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-04 18:22:06 +0200

    Update tests.py

--- fl_client/tests.py
+++ fl_client/tests.py
@@ -1,6 +1,6 @@
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
-from .views import index
+from .views import index, data_processing, data_processing_faqs, data_processing_models, data_processing_tutorials
 
 class TestIndexView(TestCase):
     def setUp(self):
@@ -11,3 +11,27 @@ class TestIndexView(TestCase):
         response = index(request)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'core/index.html')
+
+    def test_data_processing_view(self):
+        request = self.factory.get(reverse('data_processing'))
+        response = data_processing(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'data_processing/data_processing.html')
+
+    def test_data_processing_faqs_view(self):
+        request = self.factory.get(reverse('data_processing_faqs'))
+        response = data_processing_faqs(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'data_processing/data_processing_faqs.html')
+
+    def test_data_processing_models_view(self):
+        request = self.factory.get(reverse('data_processing_models'))
+        response = data_processing_models(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'data_processing/data_processing_models.html')
+
+    def test_data_processing_tutorials_view(self):
+        request = self.factory.get(reverse('data_processing_tutorials'))
+        response = data_processing_tutorials(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'data_processing/data_processing_tutorials.html')
