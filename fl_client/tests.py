502865dba106d7cb0fe3674ad85d7f28ab792ff5
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-04 18:16:01 +0200

    Update tests.py

--- fl_client/tests.py
+++ fl_client/tests.py
@@ -1,3 +1,13 @@
-# from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from django.urls import reverse
+from .views import index
 
-# Create your tests here.
+class TestIndexView(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_index_view(self):
+        request = self.factory.get(reverse('index'))
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/index.html')
