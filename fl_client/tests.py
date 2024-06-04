f24c1e3babb8e3eb921709b56a3f7917d5130adf
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-04 20:33:58 +0200

    Update tests.py

--- fl_client/tests.py
+++ fl_client/tests.py
@@ -1,34 +1,90 @@
-from django.test import RequestFactory, TestCase
-from django.urls import reverse, resolve
-from .views import (
-    index,
-    data_processing,
-    data_processing_faqs,
-    data_processing_models,
-    data_processing_tutorials,
-)
-from fl_client import views
-
+from django.test import TestCase, Client
+from django.urls import reverse
 
 class TestUrls(TestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_index_url(self):
-        path = reverse("fl_client:index")
-        self.assertEqual(resolve(path).func, views.index)
+        response = self.client.get(reverse('fl_client:index'))
+        self.assertEqual(response.status_code, 200)
 
     def test_data_processing_url(self):
-        path = reverse("fl_client:data_processing")
-        self.assertEqual(resolve(path).func, views.data_processing)
+        response = self.client.get(reverse('fl_client:data_processing'))
+        self.assertEqual(response.status_code, 200)
 
     def test_data_processing_faqs_url(self):
-        path = reverse("fl_client:data_processing_faqs")
-        self.assertEqual(resolve(path).func, views.data_processing_faqs)
+        response = self.client.get(reverse('fl_client:data_processing_faqs'))
+        self.assertEqual(response.status_code, 200)
 
     def test_data_processing_models_url(self):
-        path = reverse("fl_client:data_processing_models")
-        self.assertEqual(resolve(path).func, views.data_processing_models)
+        response = self.client.get(reverse('fl_client:data_processing_models'))
+        self.assertEqual(response.status_code, 200)
 
     def test_data_processing_tutorials_url(self):
-        path = reverse("fl_client:data_processing_tutorials")
-        self.assertEqual(resolve(path).func, views.data_processing_tutorials)
+        response = self.client.get(reverse('fl_client:data_processing_tutorials'))
+        self.assertEqual(response.status_code, 200)
 
+    def test_deep_learning_url(self):
+        response = self.client.get(reverse('fl_client:deep_learning'))
+        self.assertEqual(response.status_code, 200)
 
+    def test_deep_learning_faqs_url(self):
+        response = self.client.get(reverse('fl_client:deepl_learning_faqs'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_deep_learning_models_url(self):
+        response = self.client.get(reverse('fl_client:deep_learning_models'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_deep_learning_tutorials_url(self):
+        response = self.client.get(reverse('fl_client:deep_learning_tutorials'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_machine_learning_url(self):
+        response = self.client.get(reverse('fl_client:machine_learning'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_machine_learning_faqs_url(self):
+        response = self.client.get(reverse('fl_client:machine_learning_faqs'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_machine_learning_models_url(self):
+        response = self.client.get(reverse('fl_client:machine_learning_models'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_machine_learning_tutorials_url(self):
+        response = self.client.get(reverse('fl_client:machine_learning_tutorials'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_natural_language_processing_url(self):
+        response = self.client.get(reverse('fl_client:natural_language_processing'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_natural_language_processing_faqs_url(self):
+        response = self.client.get(reverse('fl_client:natural_language_processing_faqs'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_natural_language_processing_models_url(self):
+        response = self.client.get(reverse('fl_client:natural_language_processing_models'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_natural_language_processing_tutorials_url(self):
+        response = self.client.get(reverse('fl_client:natural_language_processing_tutorials'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_url(self):
+        response = self.client.get(reverse('fl_client:register'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_url(self):
+        response = self.client.get(reverse('fl_client:edit'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_import_url(self):
+        response = self.client.get(reverse('fl_client:import'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_download_url(self):
+        response = self.client.get(reverse('fl_client:download'))
+        self.assertEqual(response.status_code, 200)
