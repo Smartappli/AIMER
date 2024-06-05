8fb1dbcdbdd0a522b01713806e3913e1094994a0
Smartappli <26173534+Smartappli@users.noreply.github.com>
2024-06-05 17:21:38 +0000

    :art: Format Python code with psf/black

--- fl_server/tests/tests_server.py
+++ fl_server/tests/tests_server.py
@@ -8,26 +8,27 @@ from fl_server.server import launch_node, launch_nodes, load_secrets
 
 
 class TestYourModule(TestCase):
-
-    @patch('os.getenv')
-    @patch('dotenv.load_dotenv')
+    @patch("os.getenv")
+    @patch("dotenv.load_dotenv")
     def test_load_secrets(self, mock_load_dotenv, mock_getenv):
-        mock_getenv.side_effect = ['test_email', 'test_password']
+        mock_getenv.side_effect = ["test_email", "test_password"]
         email, password = load_secrets()
         mock_load_dotenv.assert_called_once()
-        self.assertEqual(email, 'test_email')
-        self.assertEqual(password, 'test_password')
+        self.assertEqual(email, "test_email")
+        self.assertEqual(password, "test_password")
 
-    @patch('your_module.sy.orchestra.launch')
+    @patch("your_module.sy.orchestra.launch")
     def test_launch_node(self, mock_launch):
         mock_node = MagicMock()
         mock_launch.return_value = mock_node
-        launch_node('test', 9000, 'test_email', 'test_password')
-        mock_launch.assert_called_once_with(name='do-test', port=9000, local_db=True, dev_mode=True, reset=True)
+        launch_node("test", 9000, "test_email", "test_password")
+        mock_launch.assert_called_once_with(
+            name="do-test", port=9000, local_db=True, dev_mode=True, reset=True
+        )
 
-    @patch('fl_server.server.load_secrets')
-    @patch('fl_server.server.launch_node')
+    @patch("fl_server.server.load_secrets")
+    @patch("fl_server.server.launch_node")
     def test_launch_nodes(self, mock_launch_node, mock_load_secrets):
-        mock_load_secrets.return_value = ('test_email', 'test_password')
+        mock_load_secrets.return_value = ("test_email", "test_password")
         launch_nodes()
         self.assertEqual(mock_launch_node.call_count, 3)
