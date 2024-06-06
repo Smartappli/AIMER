c04159df12d99ecb382797190288af65ce316739
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-06 21:32:14 +0200

    Update server.py

--- fl_server/server.py
+++ fl_server/server.py
@@ -20,7 +20,7 @@ def launch_node(name, port):
         node: The launched node.
     """
     print(f"\n--- Démarrage du noeud {name} ---")
-    node = sy.Orchestra.launch(
+    node = sy.orchestra.Orchestra.launch(
         name=name,
         port=port,
         local_db=True,
