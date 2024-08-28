460c96487b33768d460ae5ef9805ccdabfa84115
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-08-28 14:13:43 +0200

    Update hieradet_sam2.py

--- fl_common/models/hieradet_sam2.py
+++ fl_common/models/hieradet_sam2.py
@@ -1,21 +1,21 @@
 from timm import create_model
 
 
-def get_hiera_sam2_model(hiera_sam2_type, num_classes):
+def get_hieradet_sam2_model(hieradet_sam2_type, num_classes):
     """
-    Create and return a HIERA SAM2 model based on the specified architecture.
+    Create and return a HIERADET SAM2 model based on the specified architecture.
 
     Args:
-        hiera_sam2_type (str): Type of HIERA SAM2 architecture.
+        hiera_sam2_type (str): Type of HIERADET SAM2 architecture.
         num_classes (int): Number of output classes.
 
     Returns:
-        torch.nn.Module: Created HIERA SAM2 model.
+        torch.nn.Module: Created HIERADET SAM2 model.
 
     Raises:
-        ValueError: If an unknown HIERA SAM2 architecture is specified.
+        ValueError: If an unknown HIERADET SAM2 architecture is specified.
     """
-    valid_hiera_sam2_types = {
+    valid_hieradet_sam2_types = {
         "sam2_hiera_tiny",
         "sam2_hiera_small",
         "sam2_hiera_base_plus",
@@ -23,20 +23,20 @@ def get_hiera_sam2_model(hiera_sam2_type, num_classes):
         "hieradet_small",
     }
 
-    if hiera_sam2_type not in valid_hiera_sam2_types:
-        msg = f"Unknown HIERA SAM2 Architecture: {hiera_sam2_type}"
+    if hieradet_sam2_type not in valid_hieradet_sam2_types:
+        msg = f"Unknown HIERADET SAM2 Architecture: {hieradet_sam2_type}"
         raise ValueError(msg)
 
     try:
         return create_model(
-            hiera_sam2_type,
+            hieradet_sam2_type,
             pretrained=True,
             num_classes=num_classes,
         )
     except RuntimeError as e:
-        print(f"{hiera_sam2_type} - Error loading pretrained model: {e}")
+        print(f"{hieradet_sam2_type} - Error loading pretrained model: {e}")
         return create_model(
-            hiera_sam2_type,
+            hieradet_sam2_type,
             pretrained=False,
             num_classes=num_classes,
         )
