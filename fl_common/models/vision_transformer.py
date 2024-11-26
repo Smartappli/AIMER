99e0ac750f79ce522a75312591ed654c75a6e8d7
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-11-26 23:44:21 +0100

    Update vision_transformer.py

--- fl_common/models/vision_transformer.py
+++ fl_common/models/vision_transformer.py
@@ -132,6 +132,10 @@ def get_vision_transformer_model(vision_type, num_classes):
         "vit_base_patch16_reg4_gap_256",
         "vit_so150m_patch16_reg4_map_256",
         "vit_so150m_patch16_reg4_gap_256",
+        "vit_intern300m_patch14_448",
+        "test_vit",
+        "test_vit2",
+        "test_vit3",
     ]
 
     # Check if the vision type is from torchvision
