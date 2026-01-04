87a067c4b8760200e1750548fe959a31b8e9c482
odebauche <odebauche@awegroupe.be>
2026-01-04 18:11:52 +0100

    Creation of AIMER project 2026

--- AIMER/AIMER/urls.py
+++ AIMER/AIMER/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for website project.
+URL configuration for AIMER project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
