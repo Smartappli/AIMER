e30396cca7aef28a81d7d61baa79fbc5983b4b50
odebauche <odebauche@awegroupe.be>
2026-04-02 12:31:00 +0200

    pre-commit update

--- FARM/FARM/federated/__init__.py
+++ FARM/FARM/federated/__init__.py
@@ -1,11 +1,21 @@
 from .clients import ClientContext, FederatedTaskClient, RagClient
 from .data import FederatedDataset
 from .rag import RagDocument, RagIndex, RagState
-from .runner import (FederatedServerConfig, build_rag_client,
-                     build_task_client, start_rag_server, start_task_server)
+from .runner import (
+    FederatedServerConfig,
+    build_rag_client,
+    build_task_client,
+    start_rag_server,
+    start_task_server,
+)
 from .strategies import RagFedAvgStrategy, TaskFedAvgStrategy
-from .tasks import (EvaluationResult, TaskDefinition, TaskHandlers, TaskType,
-                    TrainingResult)
+from .tasks import (
+    EvaluationResult,
+    TaskDefinition,
+    TaskHandlers,
+    TaskType,
+    TrainingResult,
+)
 
 __all__ = [
     "ClientContext",
