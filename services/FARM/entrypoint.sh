3c86b7e85a08c001dfe3266b229985df158ba145
odebauche <odebauche@awegroupe.be>
2026-05-27 21:28:23 +0200

    revamp round 1

--- services/FARM/entrypoint.sh
+++ services/FARM/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -e
 
+VENV_BIN="${VIRTUAL_ENV:-/app/.venv}/bin"
+
 if [ "${RUN_DJANGO_COLLECTSTATIC:-1}" = "1" ]; then
-  uv run --locked --no-dev --no-build python manage.py collectstatic --noinput
+  "${VENV_BIN}/python" manage.py collectstatic --noinput
 fi
 
 if [ "${RUN_DJANGO_MIGRATIONS:-1}" = "1" ]; then
-  uv run --locked --no-dev --no-build python manage.py migrate --noinput
+  "${VENV_BIN}/python" manage.py migrate --noinput
 fi
 
 exec "$@"
